d89de1880984cb9a73364342c5f34bf8c284d60a
dxssssss <wwlzxukeqing@163.com>
2018-03-02 00:05:41 +0800

    200 page

--- jd_spider.py
+++ jd_spider.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 def get_comments(url):
     res = requests.get(url)
-    result = res.text.split('fetchJSON_comment98vv434(')[1].split(');')[0]
+    result = res.text.split('fetchJSON_comment98vv40575(')[1].split(');')[0]
     result = json.loads(result)
     comments = []
     comment_list = result['comments']
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     print('Spidering...')
     start_time = datetime.now()
     total_comments = []
-    url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv434&productId=4415077&score=0&sortType=5&page={}&pageSize=10&isShadowSku=0&fold=1'
-    for i in range(0, 80):
+    url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv40575&productId=3701943&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1'
+    for i in range(0, 200):
         new_url = url.format(i)
         comms = get_comments(new_url)
         total_comments.extend(comms)
